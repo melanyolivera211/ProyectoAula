5556e47447b26424cca22fc874fcf4141b12c297
melanyolivera211 <143230796+melanyolivera211@users.noreply.github.com>
2024-11-22 14:11:14 -0500

    cambio de conexión a base de datos

--- src/proyectoaula/conexionBD/ConexionUsuario.java
+++ src/proyectoaula/conexionBD/ConexionUsuario.java
@@ -3,60 +3,24 @@ package proyectoaula.conexionBD;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ConexionUsuario {
-    String currentDir = System.getProperty("user.dir");
-    String strConexionDB = "jdbc:sqlite:" + currentDir + "/BD/BD.s3db";
-    Connection conn;
 
-    public ConexionUsuario() {
-        try {
-            Class.forName("org.sqlite.JDBC");
-            conn = DriverManager.getConnection(strConexionDB);
-            System.out.println("Conexion establecida");
-        } catch (ClassNotFoundException | SQLException e) {
-            System.out.println("Error de conexion " + e);
-        }
-    }
+    Connection cn = null;
 
-    public int ejecutarSentenciaSQL(String strSentenciaSQL) {
+    public Connection conectar() {
         try {
-            PreparedStatement pstm = conn.prepareStatement(strSentenciaSQL);
-            pstm.execute();
-            return 1;
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            cn = DriverManager.getConnection("jdbc:mysql://localhost/login-proyecto", "root", "");
+            System.out.println("Conexión exitosa");
         } catch (SQLException e) {
-            System.out.println(e);
-            return 0;
+            System.out.println("Conexión fallida: " + e);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ConexionUsuario.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-    }
-
-    public ResultSet consultarRegistros(String strSentenciaSQL) {
-        try {
-            PreparedStatement pstm = conn.prepareStatement(strSentenciaSQL);
-            ResultSet respuesta = pstm.executeQuery();
-            return respuesta;
-        } catch (Exception e) {
-            System.out.println(e);
-            return null;
-        }
-
-    }
-
-    public boolean verificarInicioSesion(String cedula, String contraseña) {
-        String strSentenciaSelect = "SELECT COUNT(*) FROM Usuarios WHERE Cedula = ? AND Clave = ?";
-        try (PreparedStatement pstmt = conn.prepareStatement(strSentenciaSelect)) {
-            pstmt.setString(1, cedula);
-            pstmt.setString(2, contraseña);
-            ResultSet rs = pstmt.executeQuery();
-            int count = rs.getInt(1);
-            return count > 0; // Devuelve true si se encuentra al menos un registro con la cédula y contraseña dadas
-        } catch (SQLException e) {
-            System.out.println("Error al verificar inicio de sesión: " + e.getMessage());
-            return false;
-        }
+        return cn;
     }
 
 }
